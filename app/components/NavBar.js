fc02bbf62d3d7d8858e699127e240474b0ac26aa
shreyasrkarthik <shreyas.rkarthik@gmail.com>
2025-02-16 20:32:28 +0530

    Added Link to the icon and header.

--- app/components/NavBar.js
+++ app/components/NavBar.js
@@ -11,13 +11,13 @@ export default function NavBar() {
     <nav className="bg-gradient-to-r from-white-100 via-white-800 to-white-100 animate-gradient text-white p-1 shadow-md">
       <div className="container mx-auto flex justify-between items-center">
         {/* Logo + Title */}
-        <div className="flex items-center space-x-3">
+        <Link href="/" className="flex items-center space-x-3">
           {/* Logo (replace with your uploaded image) */}
           <Image src="/logo_no_bg.png" alt="Website Logo" width={50} height={50} className="square-full" />
 
           {/* Website Title */}
-          <span className="text-2xl font-bold text-color text-[#c8cefa]">Code, Content and Chronicles</span>
-        </div>
+          <span className="text-2xl font-bold text-[#c8cefa]">Code, Content and Chronicles</span>
+        </Link>
 
         {/* Desktop Navigation */}
         <div className="hidden md:flex space-x-4">
@@ -25,7 +25,7 @@ export default function NavBar() {
             <Link
               key={item}
               href={`/${item.toLowerCase()}`}
-              className="px-4 py-2 border border-white rounded-md hover:bg-[#c8cefa] hover:text-blue-600 transition"
+              className="px-4 py-2 border border-white rounded-md hover:bg-[#c8cefa] transition"
             >
               {item}
             </Link>
@@ -43,12 +43,12 @@ export default function NavBar() {
 
       {/* Mobile Navigation */}
       {isOpen && (
-        <div className="md:hidden flex flex-col items-center bg-blue-500 mt-2 py-2 space-y-2">
+        <div className="md:hidden flex flex-col items-center mt-2 py-2 space-y-2">
           {["Home", "Contact", "Social", "About"].map((item) => (
             <Link
               key={item}
               href={`/${item.toLowerCase()}`}
-              className="px-4 py-2 border border-white rounded-md hover:bg-[#c8cefa] hover:text-blue-600 transition"
+              className="px-4 py-2 border border-white rounded-md hover:bg-[#c8cefa] transition"
               onClick={() => setIsOpen(false)}
             >
               {item}
